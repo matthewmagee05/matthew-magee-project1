a52dbe416a8df6e24ddee54c4be55be7d557653f
matthewmagee05 <matthewmagee05@gmail.com>
2018-05-04 11:48:55 -0400

    added data serializer helper class

---
+++ RestaurantProjectMVC/Data/DataSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using RestaurantProjectMVC.Models;
+using System.IO;
+using Newtonsoft.Json;
+using System.Configuration;
+
+namespace RestaurantProjectMVC.Data
+{
+    class DataSerializer
+    {
+
+        public IList<Restaurant> myArray { get; set; }
+
+
+        public IList<Restaurant> deserialize()
+        {
+            string filePath = ConfigurationManager.AppSettings["Path"];
+            var jsonText = File.ReadAllText(filePath);
+            myArray = JsonConvert.DeserializeObject<IList<Restaurant>>(jsonText);
+            return myArray;
+        }
+
+        public string serialize()
+        {
+            var json = JsonConvert.SerializeObject(myArray);
+            return json;
+        }
+    }
+}
